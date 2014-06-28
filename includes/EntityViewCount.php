4dc50ee2ea3598c574476c0de855205831752b91
Roy Segall <roy.segall@gizra.com>
2014-06-28 19:16:41 +0300

    Adding view mode and the entity owner to the scehma.

--- includes/EntityViewCount.php
+++ includes/EntityViewCount.php
@@ -29,6 +29,20 @@ class EntityViewCount extends Entity {
   public $entity_id;
 
   /**
+   * @var String
+   *
+   * The entity owner UID.
+   */
+  public $entity_owner;
+
+  /**
+   * @var String
+   *
+   * The view mode when the entity was viewed.
+   */
+  public $view_mode;
+
+  /**
    * @var Integer
    *
    * The unix time stamp the entry created.
@@ -41,4 +55,5 @@ class EntityViewCount extends Entity {
    * The user ID that the view the entity which the entry relate to.
    */
   public $uid;
+
 }
